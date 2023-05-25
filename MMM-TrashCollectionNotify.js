1b6d884abd03692d361ea89bc99ade129b5742b1
ChrwagnerTHU <wagnch02@thu.de>
2023-05-25 17:30:17 +0200

    First Commit

---
+++ MMM-TrashCollectionNotify.js
@@ -0,0 +1,78 @@
+/* MagicMirror2 Module: TrashCollection */
+
+Module.register("MMM-TrashCollectionNotify", {
+  // Default module config
+  defaults: {
+    cityId: "",
+    areaId: "",
+    description: false,
+  },
+
+  // Module initialization
+  start: function () {
+    this.trashData = null;
+    this.updateTrashData();
+  },
+
+  // Fetches the trash collection data for the specified city and area
+  updateTrashData: function () {
+    const url = `https://mymuell.jumomind.com/mmapp/api.php?r=dates&city_id=${this.config.cityId}&area_id=${this.config.areaId}`;
+    var self = this;
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        self.trashData = JSON.parse(this.responseText);
+        self.updateDom();
+      }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+  },
+
+  // Generates the module content
+  getDom: function () {
+    const wrapper = document.createElement("div");
+  
+    if (!this.trashData) {
+      wrapper.innerHTML = "Loading...";
+      return wrapper;
+    }
+  
+    const today = new Date().toISOString().split("T")[0];
+    const todayCollections = this.trashData.filter((collection) => collection.day === today);
+  
+    if (todayCollections.length === 0) {
+      wrapper.innerHTML = "No trash collection today.";
+      return wrapper;
+    }
+  
+    const table = document.createElement("table");
+    table.className = "trash-collection-table";
+  
+    todayCollections.forEach((collection) => {
+      const row = document.createElement("tr");
+  
+      const colorCell = document.createElement("td");
+      colorCell.className = "trash-collection-color";
+      colorCell.style.backgroundColor = "#" + collection.color;
+      row.appendChild(colorCell);
+  
+      const titleCell = document.createElement("td");
+      titleCell.className = "trash-collection-title";
+      titleCell.innerHTML = collection.title;
+      row.appendChild(titleCell);
+  
+      if (self.description){
+        const descriptionCell = document.createElement("td");
+        descriptionCell.className = "trash-collection-description";
+        descriptionCell.innerHTML = collection.description;
+        row.appendChild(descriptionCell);
+      }
+  
+      table.appendChild(row);
+    });
+  
+    wrapper.appendChild(table);
+    return wrapper;
+  },
+});
--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # MMM-TrashCollectionNotify
-MagicMirror Module to display if on todays date the trash gets collected
+
+
+This a module for the [MagicMirror](https://github.com/MichMich/MagicMirror). It can display if on todays date a certain trash type is collected in Germany.
+Only cities which use the MyMüll application are available with this module. 
+Have a look [here](https://mymuell.jumomind.com/mmapp/loxone/lox.php?r=cities) if your city is supported.
+
+## Installation
+
+In terminal, go to your MagicMirror's Module folder:
+```
+cd ~/MagicMirror/modules
+```
+
+Clone this repository:
+```
+git clone https://github.com/ChrwagnerTHU/MMM-TrashCollectionNotify
+```
+
+## Updating
+
+In terminal, go to the Module's folder and pull the latest version from GitHub:
+```
+cd ~/MagicMirror/modules/MMM-TrashCollectionNotify
+git pull
+```
+
+## Configuration options
+
+### Main config
+|Option|Default|Description|
+|---|---|---|
+|`cityId`|""|ID of the city you want to display|
+|`areaId`|""|ID of the area you want to display|
+|`description`|false|Flag if further descriptions shall be displayed|
+
+
+## Using the module
+
+### Get CityID and AreaID
+
+- Look [here](https://mymuell.jumomind.com/mmapp/loxone/lox.php?r=cities) to find your cities ID
+- Look on the following link for your area ID `https://mymuell.jumomind.com/mmapp/api.php?r=streets&city_id=ID` where the ID at the end is your cities ID
+
+### Update the `config.js`
+
+To use this module, add it to the modules array in the `~/MagicMirror/config/config.js` file.
+
+#### Example
+```
+    {
+        module: 'MMM-TrashCollectionNotify',
+        header: "Abfuhrkalender",
+        position: 'top_left',
+        config: {
+            cityId: '62513',
+            areaId: '355'
+        }
+    },
+```
+
+## Special Thanks
+- [Michael Teeuw](https://github.com/MichMich) for creating the awesome [MagicMirror2](https://github.com/MichMich/MagicMirror/tree/develop) project that made this module possible.
